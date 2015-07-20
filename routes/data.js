3e91c13ae07adcc51f199fa7daef6cd6ccf419c0
yaokuo <yaokuo@letv.com>
2015-07-20 17:03:13 +0800

    add mock data

--- routes/data.js
+++ routes/data.js
@@ -1,15 +1,20 @@
 var express = require('express');
+var path = require('path');
 var mock = require('mockjs');
+var fs = require('fs');
+
+var mockPath = path.join(__dirname,'../mock_tpl');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var data = mock.mock({
-        'list|1-10': [{
-            'id|+1': 1
-        }]
-    });
-    console.log(JSON.stringify(data, null, 4));
+//    console.log(req.baseUrl);
+//    console.log('mockPath:'+mockPath+'/test.mock');
+
+    var mockTpl = fs.readFileSync(mockPath+'/test.mock').toString();
+//    console.log(JSON.parse(mockTpl));
+    var data = mock.mock(JSON.parse(mockTpl));
+//    console.log(JSON.stringify(data, null, 4));
     res.send('You request a api');
 });
 
