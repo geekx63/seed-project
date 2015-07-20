8dcab62626140226b2999b4485ebd9e5c3762e62
yaokuo <yaokuo@letv.com>
2015-07-20 18:50:56 +0800

    优化 mock

--- routes/data.js
+++ routes/data.js
@@ -3,19 +3,17 @@ var path = require('path');
 var mock = require('mockjs');
 var fs = require('fs');
 
-var mockPath = path.join(__dirname,'../mock_tpl');
+var mockPath = path.join(__dirname,'../mock_tpl/');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-//    console.log(req.baseUrl);
-//    console.log('mockPath:'+mockPath+'/test.mock');
+/* GET restAPI data. */
+router.all('/*', function(req, res, next) {
+    var fileName = req.url.split('/').join('_')+'.mock';
+    console.log(fileName);
 
-    var mockTpl = fs.readFileSync(mockPath+'/test.mock').toString();
-//    console.log(JSON.parse(mockTpl));
+    var mockTpl = fs.readFileSync(mockPath+fileName).toString();
     var data = mock.mock(JSON.parse(mockTpl));
-//    console.log(JSON.stringify(data, null, 4));
-    res.send('You request a api');
+    res.send(data);
 });
 
 module.exports = router;
